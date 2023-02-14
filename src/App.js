28dbfbef9bdd0f0e4e39dc321b1d40116e3974e7
NalinMohan61 <106859940+NalinMohan61@users.noreply.github.com>
2023-02-14 11:36:30 +0530

    Delete src directory

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import { EmployeeDetails } from "./components";
-import "./App.css";
-
-function App() {
-  return <EmployeeDetails />;
-}
-
-export default App;
--- src/components/index.jsx
+++
@@ -1,6 +0,0 @@
-import EmployeeDetails from "./EmployeeDetails";
-import EmployeeTable from "./EmployeeTable";
-import FormPopup from "./FormPopup";
-import RemovePopup from "./RemovePopup";
-
-export { EmployeeDetails, EmployeeTable, FormPopup, RemovePopup };
